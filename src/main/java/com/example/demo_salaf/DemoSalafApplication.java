9a1ecb0cd39c74e6f40ab7725a0eda216937e8bb
izabela <99564211+izabelaapm@users.noreply.github.com>
2023-08-28 19:40:52 -0300

    Add files via upload

---
+++ src/main/java/com/example/demo_salaf/DemoSalafApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_salaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSalafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSalafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_salaf/DemoSalafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_salaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSalafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
